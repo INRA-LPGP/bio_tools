b2ce625afeb520b97e22bcce7c4c37020321787c
Romain Feron <romain.feron@evobio.eu>
2017-03-20 11:07:14 +0100

    Finished qsub implementation, some fixes on cmd

--- bio_tools/main.py
+++ bio_tools/main.py
@@ -8,17 +8,18 @@ USR_PATH = './user.json'
 parameters = initialize_tool(DE_NOVO)
 user_settings = initialize_user()
 
-set_parameters_from_json(parameters, 'test.json')
-set_parameters_from_json(user_settings, 'user.json')
+set_parameters_from_json(parameters, './test/test.json')
+set_parameters_from_json(user_settings, './test/user.json')
 
 values = {N_THREADS: 5,
           MISMATCHES_IN_INDIVIDUAL: 6,
-          POPULATION_MAP: 'test.popmap'
+          POPULATION_MAP: './test/test.popmap',
+          CMD: 'denovo_map.pl'
           }
 
 set_parameters_from_dictionary(parameters, values)
 
-cmd = Command(parameters)
+cmd = Command(parameters, user_settings)
 # print(cmd.cmd())
 # cmd.run()
-cmd.generate_qsub_file(user_settings)
+# cmd.submit()
