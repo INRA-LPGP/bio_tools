eb5c2751298c9f230d393eb7a09f4a8e6b7c0497
Romain Feron <romain.feron@evobio.eu>
2017-03-24 14:28:21 +0100

    TODO changes

--- bio_tools/main.py
+++ bio_tools/main.py
@@ -3,9 +3,8 @@ from job import Job
 
 values = {'N_THREADS': 16,
           'MISMATCHES_IN_INDIVIDUAL': 6,
-          'POPULATION_MAP': './test/test.popmap'}
+          'POPULATION_MAP': ''}
 
 de_novo = Job(stacks, stacks.de_novo)
 de_novo.set_parameters(values)
-print(de_novo.cmd)
 de_novo.create_qsub_file()
