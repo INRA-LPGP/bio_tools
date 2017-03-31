5276ebae7f45689596d043f4b20dd71ea21b496d
RomainFeron <romain.feron@evobio.eu>
2017-03-31 21:40:09 +0200

    Cleaned up type checks to only include standard types. Custom types should be implemented as rules later.

--- bio_tools/parameters/parameter.py
+++ bio_tools/parameters/parameter.py
@@ -1,41 +1,38 @@
 from distutils.util import strtobool
-from . import check_types
+from . import type_checks
 from commons.types import *
 
 
 class Parameter:
     'Parameter object storing all information about a parameter'
 
-    def __init__(self, name_s, flag_s, type_s, default_s, required_s):
-        self.name = name_s
-        self.flag = flag_s
+    def __init__(self, name, flag, type_s, default, required):
+        self.name = name
+        self.flag = flag
         self.type = type_s
-        self.default = default_s
-        self.required = strtobool(required_s)
+        self.default = default
+        self.required = strtobool(required)
         self.value = None
 
-    def set_value(self, value_s):
+    def set_value(self, value):
         """
-        First check that the given value is of correct type (see check_types.py)
+        First check that the given value is of correct type (see type_checks.py)
         Then sets the parameter's value to the given value.
         """
         try:
-            self.type_is_correct(value_s)
+            self.type_is_correct(value)
             if self.type == INT:
-                self.value = (int(value_s))
+                self.value = (int(value))
             if self.type == FLOAT:
-                self.value = (float(value_s))
+                self.value = (float(value))
             elif self.type == BOOL:
-                self.value = strtobool(value_s)
+                self.value = strtobool(value)
             else:
-                self.value = value_s
+                self.value = value
         except ValueError:
-            print("The value of parameter " + self.name +
-                  " should be of type " + self.type +
-                  " but you tried to set the value to " +
-                  value_s + ".\nCheck the input for this parameter." +
-                  " For more information about value types," +
-                  " check file commons.py section Types.")
+            print('*** Error while setting the value for parameter "' +
+                  self.name + '"" : ' + self.type + ' expected, ' +
+                  'but value was "' + value + '"')
             raise ValueError
 
     def type_is_correct(self, value):
@@ -44,24 +41,24 @@ class Parameter:
         Calls a type checking function according to "type_s" parameter.
         """
         if self.type == INT:
-            return check_types.is_int(value)
-        if self.type == FLOAT:
-            return check_types.is_float(value)
+            return type_checks.is_int(value)
+        elif self.type == FLOAT:
+            return type_checks.is_float(value)
         elif self.type == DIR:
-            return check_types.is_dir(value)
+            return type_checks.is_dir(value)
         elif self.type == FILE_I:
-            return check_types.is_file_i(value)
+            return type_checks.is_file_i(value)
         elif self.type == FILE_O:
-            return check_types.is_file_o(value)
+            return type_checks.is_file_o(value)
         elif self.type == CROSS:
-            return check_types.is_cross(value)
+            return type_checks.is_cross(value)
         elif self.type == BOOL:
-            return check_types.is_bool(value)
+            return type_checks.is_bool(value)
         elif self.type == STR:
-            return check_types.is_str(value)
+            return type_checks.is_str(value)
         elif self.type == PIPE:
-            return check_types.is_pipe(value)
+            return type_checks.is_pipe(value)
         elif self.type == MAIL:
-            return check_types.is_mail(value)
+            return type_checks.is_mail(value)
         elif self.type == MEMORY:
-            return check_types.is_memory(value)
+            return type_checks.is_memory(value)
